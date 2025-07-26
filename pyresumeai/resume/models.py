01d7b7d3f900c43bbd6a81636b6daadfb2bc663a
Onur ICLI <onuricli.py@gmail.com>
2025-07-26 17:34:06 +0300

    feat(models): add WorkExperience model to resume app

--- pyresumeai/resume/models.py
+++ pyresumeai/resume/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class WorkExperience(models.model):
+    user=models.ForeignKey(User, on_delete=models.CASCADE, related_name="work_experiences")
+    title=models.CharField(max_length=100)
+    company=models.CharField(max_length=100)
+    start_date=models.DateField()
+    end_date=models.DateField(blank=True, null=True)
+    currently_working=models.BooleanField(default=False)
+    description=models.TextField(blank=True)
+    def __str__(self):
+        return f"{self.title} at {self.company}"
